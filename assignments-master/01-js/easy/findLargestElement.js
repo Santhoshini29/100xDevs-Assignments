596d1ed2837ff1c2879837dc9a580d1e7aaf0451
Santhoshini29 <adumalapellisanthoshini29@gmail.com>
2024-01-02 20:15:51 +0530

    added anagram.js answers

--- assignments-master/01-js/easy/findLargestElement.js
+++ assignments-master/01-js/easy/findLargestElement.js
@@ -4,18 +4,19 @@
   - Input: [3, 7, 2, 9, 1]
   - Output: 9
 */
-
-  
-    function findLargestElement(numbers) {
+ 
+ 
+function findLargestElement(numbers) {
         
-        let largestElement = numbers[0];
-        for (let i = 1; i < numbers.length; i++) {
-          if (numbers[i] > largestElement) {
-            largestElement = numbers[i];
-          }
-        }
-        return largestElement;
-      }
+  let largestElement = numbers[0];
+  for (let i = 1; i < numbers.length; i++) {
+    if (numbers[i] > largestElement) {
+      largestElement = numbers[i];
+    }
+  }
+  return largestElement;
+}
+
  
 
 module.exports = findLargestElement;
